f5b6d03b428bcd22fbfffdc5bf5a97d2234f2745
anonymous <krismwasgit@gmail.com>
2023-11-13 16:10:44 +0300

    added an createusername function

--- script.js
+++ script.js
@@ -80,6 +80,22 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const user = 'Steven Thomas Williams';
+
+const createUserNames = function (arr) {
+  arr.forEach(function (acct) {
+    acct.username = acct.owner
+      .toLowerCase()
+      .split(' ')
+      .map(function (el) {
+        return el[0];
+      })
+      .join('');
+  });
+};
+
+createUserNames(accounts);
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -89,7 +105,3 @@ const currencies = new Map([
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
