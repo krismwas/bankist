c456c470091f2955e98df40c7728817f6033c6d9
anonymous <krismwasgit@gmail.com>
2023-11-30 13:37:47 +0300

    localized summarised time date

--- script.js
+++ script.js
@@ -192,13 +192,19 @@ btnLogin.addEventListener('click', function (e) {
 
     const now = new Date();
 
-    const day = `${now.getDay()}`.padStart(2, '0');
-    const month = `${now.getMonth() + 1}`.padStart(2, '0');
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      weekday: 'long',
+    };
 
-    const year = now.getFullYear();
-    const hour = now.getHours();
-    const min = now.getMinutes();
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
 
     inputLoginUsername.value = inputLoginPin.value = '';
 
